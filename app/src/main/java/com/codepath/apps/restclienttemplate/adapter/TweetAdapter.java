73423b325d96ae17f044ba5f088a06476d088e69
pranitishettiwar <praniti@lumoslabs.com>
2017-10-08 20:12:49 -0700

    Implemented clickable spans

--- app/src/main/java/com/codepath/apps/restclienttemplate/adapter/TweetAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/adapter/TweetAdapter.java
@@ -1,17 +1,21 @@
 package com.codepath.apps.restclienttemplate.adapter;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.codepath.apps.restclienttemplate.R;
+import com.codepath.apps.restclienttemplate.helper.PatternEditableBuilder;
 import com.codepath.apps.restclienttemplate.models.Tweet;
 
 /**
@@ -26,7 +30,7 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder> 
 
     //Define an interface required by ViewHolder
     public interface TweetAdapterListener {
-        public void onItemSelected(View view, int position);
+        void onItemSelected(View view, int position);
     }
 
     public TweetAdapter(List<Tweet> tweets, TweetAdapterListener listener) {
@@ -59,6 +63,23 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder> 
         holder.tvTimeAgo.setText(tweet.timeAgo);
 
         Glide.with(context).load(tweet.user.getProfileImageUrl()).into(holder.ivProfileImage);
+
+        // Style clickable spans based on pattern
+        new PatternEditableBuilder().
+            addPattern(Pattern.compile("\\@(\\w+)"), Color.parseColor("#c62c09"), new PatternEditableBuilder.SpannableClickedListener() {
+                @Override
+                public void onSpanClicked(String text) {
+                    Toast.makeText(context, "Clicked username: " + text, Toast.LENGTH_SHORT).show();
+                }
+            }).into(holder.tvBody);
+
+        new PatternEditableBuilder().
+            addPattern(Pattern.compile("\\#(\\w+)"), Color.parseColor("#c62c09"), new PatternEditableBuilder.SpannableClickedListener() {
+                @Override
+                public void onSpanClicked(String text) {
+                    Toast.makeText(context, "Clicked username: " + text, Toast.LENGTH_SHORT).show();
+                }
+            }).into(holder.tvBody);
 
     }
 
@@ -94,20 +115,6 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder> 
                     }
                 }
             });
-
-            //handle row click event
-            //            itemView.setOnClickListener(new View.OnClickListener() {
-            //                @Override
-            //                public void onClick(View view) {
-            //                    if (mListener != null) {
-            //                        //get position of row element
-            //                        int position = getAdapterPosition();
-            //                        //fire the listener callback
-            //                        mListener.onItemSelected(view, position);
-            //                    }
-            //
-            //                }
-            //            });
 
         }
 
